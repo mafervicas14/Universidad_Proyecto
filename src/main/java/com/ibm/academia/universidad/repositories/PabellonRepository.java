60eaa61a8858927bb3555d74002e5b63bf263771
Mafervicas <mafervicas@ibm.com>
2022-05-16 20:18:02 -0500

    Corrección

--- src/main/java/com/ibm/academia/universidad/repositories/PabellonRepository.java
+++ src/main/java/com/ibm/academia/universidad/repositories/PabellonRepository.java
@@ -2,10 +2,11 @@ package com.ibm.academia.universidad.repositories;
 
 import com.ibm.academia.universidad.models.entities.Carrera;
 import com.ibm.academia.universidad.models.entities.Pabellon;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface PabellonRepository extends CrudRepository<Pabellon, Integer>
 {
-    //@Query("select p from Pabellon p where p.nombre = ?1 ")
+    @Query("select p from Pabellon p where p.nombre = ?1 ")
     public Iterable<Pabellon> findPabellonByName(String nombre);
 }
